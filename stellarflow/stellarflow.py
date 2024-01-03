3f1c9d9f361b688be0b8bc160287453379e51b37
iadyo <iamadyoo@gmail.com>
2024-01-03 17:20:57 +0100

    added detrending by difference (first ver)

--- stellarflow/stellarflow.py
+++ stellarflow/stellarflow.py
@@ -1,41 +1,45 @@
+from os.path import exists
+
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
-
-from os.path import exists
 
 
 class Star:
     def __init__(self, file_name: str) -> None:
         if not exists(file_name):
-            raise FileNotFoundError(f"Nie odnaleziono pliku {file_name}!")
+            raise FileNotFoundError(f'Nie odnaleziono pliku {file_name}!')
 
-        if not file_name.endswith(".dat"):
-            raise Exception(f"Plik {file_name} nie posiada rozszerzenia .dat!")
+        if not file_name.endswith('.dat'):
+            raise Exception(f'Plik {file_name} nie posiada rozszerzenia .dat!')
 
         self._file_name = file_name
 
-    def _get_data(self) -> list[list[float]]:
+    def _get_data(self) -> np.ndarray:
         time = np.array([])
         amplitude = np.array([])
 
-        with open(self._file_name, "r") as of:
+        with open(self._file_name, 'r') as of:
             lines = np.array([x.strip().split()[:2] for x in of.readlines()])
             for line in lines:
                 time = np.append(time, float(line[0]))
                 amplitude = np.append(amplitude, float(line[1]))
 
         return np.array([time, amplitude / 100])
+    
+    def detrend_by_diff(self, time) -> np.ndarray:
+        detrend = np.array([time[dd] - time[dd - 1] for dd in range(1, len(time))])
+        return detrend
 
     def show_plot(self) -> None:
-        time = self._get_data()[0]
-        ampl = self._get_data()[1]
+        time, ampl = self._get_data()
 
         plt.figure(figsize=(20, 7))
-        plt.scatter(time, ampl, color="black")
+        plt.scatter(time, ampl, color='black')
+        plt.plot(time[:-1], self.detrend_by_diff(time), ms=12, color='red')
         plt.minorticks_on()
-        plt.xlabel("Time")
-        plt.ylabel("Amplitude")
+        plt.xlabel('Time')
+        plt.ylabel('Amplitude')
         plt.show()
 
     @property
@@ -43,9 +47,9 @@ class Star:
         return self._file_name
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("file")
+    parser.add_argument('file')
     args = parser.parse_args()
 
     star = Star(args.file)
